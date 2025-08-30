5137098dab5b3eb2d5c8760d3eea3f578a515014
leventium <leventium@users.noreply.github.com>
2025-08-30 20:18:21 +0300

    Update argument parser

--- utils/bswrap/src/main.py
+++ utils/bswrap/src/main.py
@@ -1,4 +1,6 @@
+import shutil
 import argparse
+from pathlib import Path
 from runner import MultiSimRunner
 from sqlmodel import SQLModel, create_engine
 from loguru import logger
@@ -7,12 +9,26 @@ from user_config import TASK_CONFIG
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="Booksim multiple runner",
-        description="Runs multiple instances of booksim simultaneously",
+        prog="bswrap",
+        description="Booksim multiple runner: Runs multiple "
+        "instances of booksim simultaneously",
     )
-    parser.add_argument("-j", "--jobs", type=int, default=4)
-    parser.add_argument("-e", "--exec-path", type=str)
+    parser.add_argument("-e", "--exec-path", type=str, required=True,
+                        help="Path to the BookSim simulator executable.")
+    parser.add_argument("-j", "--jobs", type=int, default=1,
+                        help="Number of jobs running simulation tasks. "
+                        "Recomended to be equal to number of "
+                        "physical CPUs. [Default: 1]")
+    parser.add_argument("-d", "--configs-directory", type=str,
+                        default="configs", help="Path to the directory "
+                        "where simulation configs are stored. "
+                        "[Default: 'configs']")
     args = parser.parse_args()
+
+    configs_dir = Path(args.configs_directory).absolute()
+    if configs_dir.exists():
+        shutil.rmtree(configs_dir)
+    configs_dir.mkdir()
 
     logger.level("INFO")
     engine = create_engine("sqlite:///sim_result.db")#, echo=True)
